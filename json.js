41cb8434ac7dae2c2fddc7aef9c9dba9f50415ea
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2025-04-18 05:47:06 +0700

    Create json.js

---
+++ json.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+// Data akun-akun kamu (JSON)
+const raw = [
+{
+    "accessToken": "",
+    "refreshToken": ""
+}
+];
+
+// Data receiver wallet & webhook
+const RECEIVER = "0x103D1D8d46de2E7829Ad5FBe2D322c705B602780";
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/";
+
+try {
+  const authJson = JSON.stringify(raw);
+  const output = `
+AUTH_JSON='${authJson}'
+RECEIVER=${RECEIVER}
+DISCORD_WEBHOOK=${DISCORD_WEBHOOK}
+  `.trim();
+
+  fs.writeFileSync('.env', output + '\n');
+  console.log('\n✅ Saved to .env');
+} catch (err) {
+  console.error('❌ Error:', err.message);
+}
